2019992b3c046b65b7014d213c4ee541cb538201
Christian Meter <christian.meter@uni-duesseldorf.de>
2016-09-16 21:04:47 +0200

    Add comment

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject reveal-cljs "0.1.0-SNAPSHOT"
-  :description "FIXME: write this!"
+  :description "A reveal.js wrapper for ClojureScript."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
