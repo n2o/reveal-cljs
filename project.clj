edaa357ab2b7af6599e759b99a55ed52adaa3f97
Christian Meter <christian.meter@uni-duesseldorf.de>
2018-12-16 22:06:10 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject reveal-cljs "0.1.27"
+(defproject reveal-cljs "1.0.0"
   :description "A reveal.js wrapper in ClojureScript."
   :url "https://github.com/n2o/reveal-cljs"
   :license {:name "MIT"
@@ -7,14 +7,12 @@
   :min-lein-version "2.6.1"
 
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/clojurescript "1.9.946"]
-                 [org.clojure/core.async "0.3.465" :exclusions [org.clojure/tools.reader]]
+                 [org.clojure/clojurescript "1.10.439"]
+                 [org.clojure/core.async "0.4.490" :exclusions [org.clojure/tools.reader]]
                  [hiccups "0.3.0"]]
 
-  :plugins [[lein-ancient "0.6.10"]
-            [lein-figwheel "0.5.13"]
-            [lein-cljsbuild "1.1.3" :exclusions [[org.clojure/clojure]]]
-            [lein-set-version "0.4.1"]]
+  :plugins [[lein-figwheel "0.5.17"]
+            [lein-cljsbuild "1.1.3" :exclusions [[org.clojure/clojure]]]]
 
   :source-paths ["src"]
 
@@ -53,50 +51,10 @@
                            :optimizations :advanced
                            :pretty-print false}}]}
 
-  :figwheel {;; :http-server-root "public" ;; default and assumes "resources"
-             ;; :server-port 3449 ;; default
-             ;; :server-ip "127.0.0.1"
+  :figwheel {:css-dirs ["resources/public/css"]}
 
-             :css-dirs ["resources/public/css"] ;; watch and update CSS
-
-             ;; Start an nREPL server into the running figwheel process
-             ;; :nrepl-port 7888
-
-             ;; Server Ring Handler (optional)
-             ;; if you want to embed a ring handler into the figwheel http-kit
-             ;; server, this is for simple ring servers, if this
-
-             ;; doesn't work for you just run your own server :) (see lein-ring)
-
-             ;; :ring-handler hello_world.server/handler
-
-             ;; To be able to open files in your editor from the heads up display
-             ;; you will need to put a script on your path.
-             ;; that script will have to take a file path and a line number
-             ;; ie. in  ~/bin/myfile-opener
-             ;; #! /bin/sh
-             ;; emacsclient -n +$2 $1
-             ;;
-             ;; :open-file-command "myfile-opener"
-
-             ;; if you are using emacsclient you can just use
-             ;; :open-file-command "emacsclient"
-
-             ;; if you want to disable the REPL
-             ;; :repl false
-
-             ;; to configure a different figwheel logfile path
-             ;; :server-logfile "tmp/logs/figwheel-logfile.log"
-             }
-
-
-  ;; setting up nREPL for Figwheel and ClojureScript dev
-  ;; Please see:
-  ;; https://github.com/bhauman/lein-figwheel/wiki/Using-the-Figwheel-REPL-within-NRepl
-
-
-  :profiles {:dev {:dependencies [[binaryage/devtools "0.9.4"]
-                                  [figwheel-sidecar "0.5.13"]
+  :profiles {:dev {:dependencies [[binaryage/devtools "0.9.10"]
+                                  [figwheel-sidecar "0.5.17"]
                                   [com.cemerick/piggieback "0.2.2"]]
                    ;; need to add dev source path here to get user.clj loaded
                    :source-paths ["src" "dev"]
